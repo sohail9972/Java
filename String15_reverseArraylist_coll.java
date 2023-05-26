49244d5832e3716c7ed015bb3efa4bb5640e301a
Sohail Maidargi <smaidargi@onetrust.com>
2023-05-26 11:40:21 +0530

    push

--- String15_reverseArraylist_coll.java
+++ String15_reverseArraylist_coll.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class String15_reverseArraylist_coll {
+
+    public static void main(String[] args) {
+        List<Integer>list=new ArrayList<>();
+        for (int i=0;i< list.size();i++){
+            list.add(i);
+        }
+    }
 }
