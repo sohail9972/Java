b993de17410bb165e7ed16c552e7f25e8a883fd2
sohail9972 <sohailmaidargirici@gmail.com>
2024-03-13 19:53:32 +0530

    Local branch Changes for Naveen Auto

---
+++ NewJourney/virtualcapacityofarrayList.java
@@ -0,0 +1,12 @@
+package NewJourney;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class virtualcapacityofarrayList {
+    public static void main(String[] args) {
+        ArrayList<Objects> ar= new ArrayList<>(20);//Declares the virtual capacity to be 20
+        System.out.println(ar.size());//Prints the Physical Capacity of the Array PC=0
+
+    }
+}
